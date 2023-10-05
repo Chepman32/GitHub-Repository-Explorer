02062359f9047f3ddf89574470f38f5a19a912c0
Chepman32 <antonkerch555@gmail.com>
2023-10-05 15:41:44 +0300

    Readme and title

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {Routes, Route} from 'react-router-dom'
 import {HomePage} from './pages/HomePage'
 import {FavouritesPage} from './pages/FavouritesPage'
 import {Navigation} from './components/Navigation'
 
 function App() {
+  useEffect(() => {
+    document.title = `Anton C. - Github search`;
+}, []);
   return (
     <>
+      <title>
+        Github search
+      </title>
       <Navigation />
       <Routes>
         <Route path="/" element={ <HomePage /> } />
